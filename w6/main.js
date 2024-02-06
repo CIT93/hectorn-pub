5a03281f256e5a7ff3e930375bf412235fa51d1d
Hector Natareno <natareno.hector@gmail.com>
2024-02-05 20:03:04 -0800

    setup done

---
+++ w6/main.js
@@ -0,0 +1,86 @@
+const FORM = document.getElementById("form");
+const OUTPUT = document.getElementById("output");
+const cfpData = [];
+
+function determineHouseSizePts(size) {
+  let houseSizePoints = 0;
+  if (size === "large") {
+    houseSizePoints = 10;
+  } else if (size === "medium") {
+    houseSizePoints = 7;
+  } else if (size === "small") {
+    houseSizePoints = 4;
+  } else if (size === "apt") {
+    houseSizePoints = 2;
+  }
+  return houseSizePoints;
+}
+
+function determineHouseHoldPts(numberInHousehold) {
+  let houseHoldPts = 0;
+  if (numberInHousehold === 1) {
+    houseHoldPts = 14;
+  } else if (numberInHousehold === 2) {
+    houseHoldPts = 12;
+  } else if (numberInHousehold === 3) {
+    houseHoldPts = 10;
+  } else if (numberInHousehold === 4) {
+    houseHoldPts = 8;
+  } else if (numberInHousehold === 5) {
+    houseHoldPts = 6;
+  } else if (numberInHousehold === 6) {
+    houseHoldPts = 4;
+  } else if (numberInHousehold > 6) {
+    houseHoldPts = 2;
+  }
+  return houseHoldPts;
+}
+
+function start(houseHoldMembers, houseSize) {
+  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+  const houseSizePTS = determineHouseSizePts(houseSize);
+  const total = houseHoldPTS + houseSizePTS;
+  cfpData.push({
+    houseM: houseHoldMembers,
+    houseS: houseSize,
+    houseMPTS: houseHoldPTS,
+    houseSPTS: houseSizePTS,
+    cfpTotal: total,
+  });
+}
+
+function displayOutput() {
+  for (obj of cfpData) {
+    console.log(obj);
+    const newH2 = document.createElement("h2");
+    newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
+    const newH3 = document.createElement("h3");
+    newH3.textContent = `Based on number in and size of home`;
+    const newP = document.createElement("p");
+    newP.textContent = `This number is based on the number if people in the house of ${obj.houseM} (Score: ${obj.houseMPTS}),`;
+    newP.textContent += ` and a ${obj.houseS} size home (Score:${obj.houseSPTS}).`;
+    OUTPUT.appendChild(newH2);
+    OUTPUT.appendChild(newH3);
+    OUTPUT.appendChild(newP);
+  }
+}
+
+FORM.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const firstname = FORM.firstname.value;
+  const lastname = FORM.lastname.value;
+  const houseMembers = parseInt(FORM.housem.value);
+  const houseSize = FORM.houses.value;
+  start(houseMembers, houseSize);
+  OUTPUT.innerHTML = "";
+  displayOutput();
+  FORM.reset();
+});
+
+
+// is the apartment score correct
+// code is not correct, apartment has a value of "apartment instead of "apt". 
+
+//why are we doing all this work in the form to make sure the user gives us good data?
+// we want to save time opn having to code this into java script and to make it as easy to confirm we are getting the data that we need from the user
