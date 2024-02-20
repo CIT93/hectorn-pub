5d0f550f30532460fed29b6b8f55dd37c19d60fc
Hector Natareno <natareno.hector@gmail.com>
2024-02-19 19:36:55 -0800

    update to code

--- w7/main.js
+++ w7/main.js
@@ -4,41 +4,6 @@ import { determineHouseHoldPts,determineHouseSizePts } from "./footprint.js";
 const OUTPUT = document.getElementById("output");
 const cfpData = [];
 
-function determineHouseSizePts(size) {
-  let houseSizePoints = 0;
-  if (size === "large") {
-    houseSizePoints = 10;
-  } else if (size === "medium") {
-    houseSizePoints = 7;
-  } else if (size === "small") {
-    houseSizePoints = 4;
-  } else if (size === "apt") {
-    houseSizePoints = 2;
-  }
-  return houseSizePoints;
-}
-
-function determineHouseHoldPts(numberInHousehold) {
-  let houseHoldPts = 0;
-  if (numberInHousehold === 1) {
-    houseHoldPts = 14;
-  } else if (numberInHousehold === 2) {
-    houseHoldPts = 12;
-  } else if (numberInHousehold === 3) {
-    houseHoldPts = 10;
-  } else if (numberInHousehold === 4) {
-    houseHoldPts = 8;
-  } else if (numberInHousehold === 5) {
-    houseHoldPts = 6;
-  } else if (numberInHousehold === 6) {
-    houseHoldPts = 4;
-  } else if (numberInHousehold > 6) {
-    houseHoldPts = 2;
-  }
-  return houseHoldPts;
-}
-
->>>>>>> d4fdf652531abb7cc5ea758bfa3ae3aabe7eb964
 function start(firstname, lastname, houseHoldMembers, houseSize) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePTS = determineHouseSizePts(houseSize);
