345b2d4833ed809e3da37eb6e6f63fb802ff2731
Hector <natareno.hector@gmail.com>
2024-03-08 04:29:16 -0800

    setup done

---
+++ w10/main.js
@@ -0,0 +1,61 @@
+import { renderTbl } from "./render.js";
+import { determineHouseHoldPts, determineHouseSizePts } from "./cfp.js";
+import { FORM, FNAME, LNAME, SUBMIT } from "./global.js";
+import { saveLS, cfpData } from "./storage.js";
+//function now has name and given const so we don't accidentally overwrite
+const start = function(first, last, houseHoldMembers, houseSize) {
+  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+  const houseSizePTS = determineHouseSizePts(houseSize);
+  const total = houseHoldPTS + houseSizePTS;
+  cfpData.push({
+    firstName: first,
+    lastName: last,
+    houseM: houseHoldMembers,
+    houseS: houseSize,
+    houseMPTS: houseHoldPTS,
+    houseSPTS: houseSizePTS,
+    cfpTotal: total,
+  });
+}
+
+renderTbl(cfpData); //passing in reference to local data and renders table
+
+// Function to validate a single field
+const validateField = function(event) {
+  const field = event.target.value;
+  const fieldId = event.target.id;
+  const fieldError = document.getElementById(`${fieldId}Error`);
+
+  if (field === "") {
+    //if whatever is in field is blank make it into template string
+    fieldError.textContent = `${fieldId} is required`;
+    //add the invalid to the classlist ("error" - listed in HTML)
+    event.target.classList.add("invalid");
+  } else {
+    fieldError.textContent = "";
+    event.target.classList.remove("invalid");
+  }
+};
+// Attach blur event listeners
+FNAME.addEventListener("blur", validateField);
+LNAME.addEventListener("blur", validateField);
+
+FORM.addEventListener("submit", function (e) {
+  e.preventDefault();
+  //setting up two variables to verify if true or false(first name is valid = true or false based on evaluating if input is valid )
+
+  //if statement to check validiti, if one of the two is invalid code will skip to else
+  if (FNAME.value !== "" && LNAME.value !== "") {
+    //if first and last name is valid we will run rest of code below - good form submission.
+    SUBMIT.textContent = ""; // remove the invalid warning when resubmitting
+    start(FNAME.value, LNAME.value, parseInt(FORM.housem.value), FORM.houses.value);
+    saveLS(cfpData); //calling function from local storage but only what is in volatile memory
+    renderTbl(cfpData);
+    FORM.reset();
+    //want to show user there is an error with their form submission
+
+  } else {
+    //text content is set as a value(submitEl) so we can clear
+    SUBMIT.textContent = "Form requires First and Last name";
+  }
+});
---
+++ w10/storage.js
@@ -0,0 +1,26 @@
+// function to save local storage. will be running function every time new user input is gathered.
+
+// variable of serializedArray will be stringify cfpData
+const saveLS = function (cfpData) {
+  const serializedArr = JSON.stringify(cfpData);
+  localStorage.setItem("cfp", serializedArr); //Key of: cfpData with Value: serialized array
+}; // when we call the the save LS we willo be passing a reference to the array
+
+// companion function - unsure if this is callback because this function is within the const cfpData so i want to say it is
+const getLS = function () {
+  //passing in the reference "cfpData"
+  const retrievedArr = localStorage.getItem("cfp"); // telling local storage to send me whatever is marked with that key"cfpData"
+
+  if (retrievedArr !== null) {
+    //not equal to null - which is empty or unknown value
+    return JSON.parse(retrievedArr); // if what you bring is not null (something IS in therewhen bringing it back) parse the relieved array and return it to be used into code
+  } else {
+    return []; //if equal to null, bring back empty array
+  }
+};
+
+//const cfpData is set up at the end because code will run line by line we would be trying to run getLS before it had the information of the function
+const cfpData = getLS(); //function is getting data from local storage
+
+
+export { cfpData, saveLS, getLS };
